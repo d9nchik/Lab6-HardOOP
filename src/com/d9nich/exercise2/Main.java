390b58ac09f21d08f5c90c36a1db40fac11a95b9
Danylo Halaiko <dan.galaiko@gmail.com>
2020-05-22 19:59:24 +0300

    Now, I have done main part.

--- src/com/d9nich/exercise2/Main.java
+++ src/com/d9nich/exercise2/Main.java
@@ -1,17 +1,50 @@
 package com.d9nich.exercise2;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+    private final static boolean IS_COMPANY_INTERFACE = false;
+    private final static File PARENT_FILE = new File("parent.dat");
+    private final static File TEACHER_FILE = new File("events.dat");
+
     public static void main(String[] args) {
-        JoyEvent[] events = JoyEvent.generateEventsForCurrentMonth(15);
-        printInFileGeneratedEvents(events, new File("events.dat"));
-        myArrayPrint(events);
+        if (IS_COMPANY_INTERFACE) {
+            JoyEvent[] events = JoyEvent.generateEventsForCurrentMonth(15);
+            printInFile(events, TEACHER_FILE);
+            myArrayPrint(events);
+        } else {
+            HashMap<String, boolean[]> parentList = readFromFile(PARENT_FILE);
+            if (parentList == null)
+                parentList = new HashMap<>();
+            JoyEvent[] events = readFromFile(TEACHER_FILE);
+            assert events != null;
+            myArrayPrint(events);
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            String patternName = input.nextLine();
+            if (parentList.containsKey(patternName)) {
+                System.out.println("You have already added your name!");
+                System.out.println("Your events choice:");
+                boolean[] choices = parentList.get(patternName);
+                for (int i = 0; i < choices.length; i++) {
+                    if (choices[i])
+                        System.out.print((i + 1) + " ");
+                }
+                System.out.println();
+                System.out.print("Do you want to change your choices?(y, n): ");
+                if (input.nextLine().charAt(0) == 'y')
+                    addParent(parentList, patternName, input, events.length);
+            } else
+                addParent(parentList, patternName, input, events.length);
+            printInFile(parentList, PARENT_FILE);
+        }
     }
 
-    private static void printInFileGeneratedEvents(JoyEvent[] events, File file) {
+    private static <E extends Serializable> void printInFile(E elements, File file) {
         try (ObjectOutputStream output = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
-            output.writeObject(events);
+            output.writeObject(elements);
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         } catch (IOException ex) {
@@ -25,9 +58,10 @@ public class Main {
             System.out.println(counter++ + ") " + element);
     }
 
-    private static JoyEvent[] readFromFileEvents(File file) {
+    @SuppressWarnings("unchecked")
+    private static <E extends Serializable> E readFromFile(File file) {
         try (ObjectInputStream input = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
-            return (JoyEvent[]) input.readObject();
+            return (E) input.readObject();
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         } catch (IOException ex) {
@@ -36,5 +70,14 @@ public class Main {
             System.out.println("Problem with class reading");
         }
         return null;
+    }
+
+    private static void addParent(HashMap<String, boolean[]> map, String value, Scanner input, int maximumBooleanSize) {
+        System.out.print("Enter number of events, which you would like to visit: ");
+        boolean[] booleans = new boolean[maximumBooleanSize];
+        String[] numbers = input.nextLine().split("[ ]+");
+        for (String number : numbers)
+            booleans[Integer.parseInt(number) - 1] = true;
+        map.put(value, booleans);
     }
 }
